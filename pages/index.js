2ae475da170a305de523c3aaacd33bc5bfb751c1
jubak01 <131758089+jubak01@users.noreply.github.com>
2023-11-06 09:55:15 +0100

    new

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import HeaderMarketPlace from '../components/HeaderMarketPlace';
+import { HeaderMarketPlace } from '../devlink/HeaderMarketPlace';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <HeaderMarketPlace></HeaderMarketPlace>
+      <HeaderMarketPlace/>
     </div>
   );
 }
